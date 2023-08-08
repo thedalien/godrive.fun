858c7bff253955c6caae84307f0987608b5e22d8
Tim Eberhard <tim.eberhard@proton.me>
2023-08-08 23:05:38 +0200

    comments for imports / may use them in future

--- client/src/components/cars/Cars.jsx
+++ client/src/components/cars/Cars.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Carousel from "../carousel/Carousel";
 
 import './Cars.css';
