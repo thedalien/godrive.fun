1d405d3340e28c4f643632fcbc969606f1af5d74
Tim Eberhard <tim.eberhard@proton.me>
2023-08-08 23:04:32 +0200

    add CarCard to the Carousel

--- client/src/components/carousel/Carousel.jsx
+++ client/src/components/carousel/Carousel.jsx
@@ -1,5 +1,7 @@
 import Flickity from 'react-flickity-component';
 import './Flickity.min.css';
+import './Carousel.css';
+import CarCard from '../carcard/CarCard';
 
 const flickityOptions = {
     initialIndex: 1,
@@ -11,6 +13,8 @@ const flickityOptions = {
     imagesLoaded: true
 }
 
+// Create map for CarCard to display Cars in the carousel
+
 export default function Carousel() {
   return (
     <Flickity
@@ -21,21 +25,8 @@ export default function Carousel() {
       reloadOnUpdate // default false
       static // default false
     >
-      <img src="https://toppng.com/uploads/preview/tesla-model-x-11562971244z3uhlgfqpe.png"/>
-      <img src="https://w7.pngwing.com/pngs/268/386/png-transparent-tesla-roadster-tesla-model-s-car-tesla-model-x-2017-tesla-model-s-p100d-compact-car-sedan-car.png"/>
-      <img src="https://e7.pngegg.com/pngimages/984/295/png-clipart-tesla-tesla.png"/>
+      <CarCard />
+      
     </Flickity>
   )
 }
-
-
-        {/* <h1>
-          Are you looking for a Car to rent?
-          <br />We're happy to help you with your needs!
-        </h1>
-        <Link to="#cars" id="goToCars">
-          <p>
-            Go to Cars
-            <FontAwesomeIcon icon="fa-solid fa-chevron-down" />
-          </p>
-        </Link> */}
