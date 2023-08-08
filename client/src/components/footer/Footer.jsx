e81f85045f739b6a24674bd07b36b12d54d917e0
Tim Eberhard <tim.eberhard@proton.me>
2023-08-08 23:38:02 +0200

    add Lorem

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,8 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div id="footer">
-        Footer
-
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias, molestiae modi odit debitis eius doloremque voluptatibus molestias, aspernatur temporibus non voluptatum doloribus nesciunt, accusamus praesentium veritatis iste consectetur obcaecati.
     </div>
   )
 }
