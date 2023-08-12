b9ac01c7b258abf9b4da395f85b022f0354d77a3
Tim Eberhard <tim.eberhard@proton.me>
2023-08-13 00:35:01 +0200

    Add CarSlider for CarCard

--- client/src/components/carcard/CarCard.jsx
+++ client/src/components/carcard/CarCard.jsx
@@ -1,10 +1,11 @@
 import './CarCard.css'
+import CarSlider from '../carslider/CarSlider';
 
 export default function CarCard() {
   return (
     <div className="carCard">
       <h1>Skoda Fabia{/* carName */}</h1>
-      <img src="https://skodavisualizer.blob.core.windows.net/skoda3d/fabia.jpg" /* carImg */ />
+      <CarSlider />
       <div className="carDetails">
         <p>Some Data here{/* carData */}</p>
         <p>Some more Data{/* carData */}</p>
