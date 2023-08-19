8e1abb788686a66525719c74c59ce218bc1c2efd
Tim Eberhard <tim.eberhard@proton.me>
2023-08-19 17:28:30 +0200

    Add CarDetailPage Elements and Fetch

--- client/src/routes/CarDetailPage.jsx
+++ client/src/routes/CarDetailPage.jsx
@@ -1,10 +1,38 @@
-import ImageSlider from "../components/imageslider/ImageSlider"
+import { useState, useEffect } from "react";
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import ImageSlider from "../components/imageslider/ImageSlider";
 
 export default function CarDetailPage() {
+  const serverURL = useSelector((state) => state.app.serverURL);
+  const { id } = useParams();
+  const [carData, setCarData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${serverURL}/api/car/get-car/${id}`)
+      .then((res) => {
+        setCarData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [id]);
+
   return (
-    <div>
-        <h1>Car name</h1>
-        <ImageSlider />
+    <div className="carCard">
+      {carData && (
+        <>
+          <h1>{carData.brand} {carData.model}</h1>
+          <ImageSlider />
+          <div className="carDetails">
+            <p>{carData.year}</p>
+            <p>{carData.dayPrice}</p>
+            <p>{carData.licensePlate}</p>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
