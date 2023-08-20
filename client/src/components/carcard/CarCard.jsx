ab987059b3d769f1f152dc3d46ea3bff612f1680
Daniel Prema <dalienprema@gmail.com>
2023-08-21 00:15:46 +0200

    err fix test

--- client/src/components/carcard/CarCard.jsx
+++ client/src/components/carcard/CarCard.jsx
@@ -10,14 +10,14 @@ export default function CarCard({ car }) {
     navigate(`/cars/${car.id}`); 
   }
   if (!car) {
-    return <div>Loading...</div>
+    return null;
   }
 
   return (
     <div className="carCard">
       <h1>{car.brand} {car.model}</h1>
-      <ImageSlider images={car.images} />
-      {/* <img src={car.images[0].url} alt="car" /> */}
+      {/* <ImageSlider images={car.images} /> */}
+      <img src={car.images[0].url} alt="car" />
       <div className="carDetails">
         <p>{car.year}</p>
         <p>{car.dayPrice}</p>
