48036261229235c86975698f5d0fca022fd9d45d
Daniel Prema <dalienprema@gmail.com>
2023-08-22 22:09:18 +0200

    hosting init

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+    port: 80,
+    }
 })
